59be1596f0c3c334f8890310c6a68c321053a6d8
jofrance <jofrance@microsoft.com>
2025-03-26 20:40:42 -0600

    adding logfile

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# Add a FileHandler to also log to a file named "app.log"
+file_handler = logging.FileHandler("app.log")
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 # ------------------
 # Configuration Setup (Managed Identity)
 # ------------------
